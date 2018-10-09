9214c748bd38e33b96510e323c3d31a9040eee3b
Jeff <coachmullen@gmail.com>
2018-10-09 16:05:38 -0700

    CSS issue resolved along with other updates.

--- js/app.js
+++ js/app.js
@@ -1,15 +1,11 @@
 'use strict';
-alert('Welcome! Answer these questions and I promise your life will be enriched.');
+alert('Welcome!');
 
 var username = prompt('What is your name?');
 
-alert('Hello ' + username + '. As we go forward, please answer with either "yes" or "no", or "y" or "n" if you don\'t feel like typing the extra letter or two...');
+alert('Hello ' + username + '. Guess what? If you answer the following 5 questions I promise your life will be enriched. To get the best results, please answer with either a "yes" or a "no"...or "y" or "n" if you need to save your strength for the coding you\'ll be doing later.');
 
 var answerOne = prompt('Am I nice').toLowerCase();
-var answerTwo = prompt('Who').toLowerCase();
-var answerThree = prompt('What').toLowerCase();
-var answerFour = prompt('When').toLowerCase();
-var answerFive = prompt('Why').toLowerCase();
 
 if (answerOne === 'yes' || answerOne === 'y') {
     alert('That\'s correct!');
@@ -19,6 +15,8 @@ if (answerOne === 'yes' || answerOne === 'y') {
     console.log('The user got question 1 wrong.');
 }
 
+var answerTwo = prompt('Who').toLowerCase();
+
 if (answerTwo === 'yes' || answerTwo === 'y') {
     alert('Good answer.');
     console.log('The user got question 2 correct');
@@ -26,3 +24,11 @@ if (answerTwo === 'yes' || answerTwo === 'y') {
     alert('Incorrect!');
     console.log('The user got question 2 wrong.');
 }
+
+var answerThree = prompt('What').toLowerCase();
+
+
+var answerFour = prompt('When').toLowerCase();
+
+
+var answerFive = prompt('Why').toLowerCase();
