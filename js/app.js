293775ba213249328527a7924cc9feb541c4df72
Jeff <coachmullen@gmail.com>
2018-10-09 15:46:05 -0700

    Question 2 is added. Still resolving minor git push error message issue.

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
 'use strict';
 alert('Welcome! Answer these questions and I promise your life will be enriched.');
-alert('Please answer with either "yes" or "no", or "y" or "n" if you\'re lazy');
+
 var username = prompt('What is your name?');
+
+alert('Hello ' + username + '. As we go forward, please answer with either "yes" or "no", or "y" or "n" if you don\'t feel like typing the extra letter or two...');
 
 var answerOne = prompt('Am I nice').toLowerCase();
 var answerTwo = prompt('Who').toLowerCase();
@@ -15,4 +17,12 @@ if (answerOne === 'yes' || answerOne === 'y') {
 } else {
     alert('Wrong!');
     console.log('The user got question 1 wrong.');
+}
+
+if (answerTwo === 'yes' || answerTwo === 'y') {
+    alert('Good answer.');
+    console.log('The user got question 2 correct');
+} else {
+    alert('Incorrect!');
+    console.log('The user got question 2 wrong.');
 }
