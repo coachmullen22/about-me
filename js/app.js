79681553a6d1c9edfc066bd9b8d8924ac15cf874
Jeff <coachmullen@gmail.com>
2018-10-09 17:14:42 -0700

    All five questions are completed.

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ alert('Welcome!');
 
 var username = prompt('What is your name?');
 
-alert('Hello ' + username + '. Guess what? If you answer the following 5 questions I promise your life will be enriched. To get the best results, please answer with either a "yes" or a "no"...or "y" or "n" if you need to save your strength for the coding you\'ll be doing later.');
+alert('Hello ' + username + '. Guess what? If you answer the following 5 questions I promise your life will be enriched. To get the best results, please answer with either a "yes" or a "no". Or if you need to save your strength for the coding you\'ll be doing later, just type "y" or "n".');
 
 var answerOne = prompt('Am I nice').toLowerCase();
 
@@ -36,5 +36,22 @@ if (answerTwo === 'yes' || answerTwo === 'y') {
 }
 var answerFour = prompt('When').toLowerCase();
 
+if (answerFour === 'yes' || answerFour === 'y') {
+    alert('Truly, you have a dizzying intellect.');
+    console.log('The user got question 4 correct.');
+} else {
+    alert('Why am I smiling? Because I know someting you do not know...I am not left-handed');
+    console.log('The user got question 4 wrong.');
+}
 
 var answerFive = prompt('Why').toLowerCase();
+
+if (answerFive === 'no' || answerFive === 'n') {
+    alert('');
+    console.log('The user got question 5 correct, although maybe they cheated...');
+} else {
+    alert('On a scale of 1 to 10, you got this one wrong.');
+    console.log('The user got question 5 incorrect.');
+}
+
+alert('Now wasn\'t that an enriching experience? Haha, I think I\'ll leave that as a rhetorical question!');
