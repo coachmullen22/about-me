5f040d9c1fd839798bcfb94417dad2a20e918905
Jeff <coachmullen@gmail.com>
2018-10-11 15:36:25 -0700

    Fixed issues

--- js/app.js
+++ js/app.js
@@ -13,7 +13,9 @@ var username = prompt('What is your name?');
 alert('Well hello, ' + username + '. Guess what? If you answer the following 7 questions I promise your life will be enriched.');
 
 alert('To get the best results, please answer with either a "yes" or a "no". Or if you need to save your strength for all the shredding you\'ll be doing later, just type "y" or "n".');
+// Q1
 
+function q1() {
 var ballPlayer = prompt('Let\'s start easy. Do I look like a baseball player?').toLowerCase();
 
 if (ballPlayer === 'yes' || ballPlayer === 'y') {
@@ -28,7 +30,12 @@ if (ballPlayer === 'yes' || ballPlayer === 'y') {
   console.log('The user got question 1 wrong.');
   console.log('The user has answered ' + correctAnswers + ' out of ' + questionsAsked + ' questions correctly.');
 }
+} 
+q1();
 
+//Q2
+
+function q2() {
 var homeRun = prompt('Did I ever hit a homerun in my high school playing days?').toLowerCase();
 
 if (homeRun === 'no' || homeRun === 'n') {
@@ -43,7 +50,12 @@ if (homeRun === 'no' || homeRun === 'n') {
   console.log('The user got question 2 wrong.');
   console.log('The user has answered ' + correctAnswers + ' out of ' + questionsAsked + ' questions correctly.');
 }
+}
+q2();
 
+//Q3
+
+function q3() {
 var marinersFan = prompt('Are the Mariners my favorite MLB team?').toLowerCase();
 
 if (marinersFan === 'yes' || marinersFan === 'y') {
@@ -57,7 +69,13 @@ if (marinersFan === 'yes' || marinersFan === 'y') {
   questionsAsked++;
   console.log('The user answered question 3 incorrectly');
   console.log('The user has answered ' + correctAnswers + ' out of ' + questionsAsked + ' questions correctly.');
-}
+} 
+} 
+q3();
+
+//Q4
+
+function q4(){
 var rightHanded = prompt('Am I right-handed?').toLowerCase();
 
 if (rightHanded === 'yes' || rightHanded === 'y') {
@@ -72,7 +90,12 @@ if (rightHanded === 'yes' || rightHanded === 'y') {
   console.log('The user got question 4 wrong.');
   console.log('The user has answered ' + correctAnswers + ' out of ' + questionsAsked + ' questions correctly.');
 }
+}
+q4();
 
+//Q5
+
+function q5() {
 var princessBride = prompt('Is it ever possible to overquote, "The Princess Bride"?').toLowerCase();
 
 if (princessBride === 'no' || princessBride === 'n') {
@@ -87,11 +110,12 @@ if (princessBride === 'no' || princessBride === 'n') {
   console.log('The user got question 5 incorrect.');
   console.log('The user has answered ' + correctAnswers + ' out of ' + questionsAsked + ' questions correctly.');
 }
-//alert('Now wasn\'t that an enriching experience? Haha, maybe we should let that be a rhetorical question!');
+}
+q5();
 
 //Question #6
 
-
+function q6() {
 var stadiumsVisited = ['indians', 'giants', 'athletics', 'padres', 'white sox', 'yankees', 'cardinals', 'brewers', 'cubs'];
 console.log(stadiumsVisited);
 //console.log(stadiumsVisited.length);
@@ -125,10 +149,14 @@ while (guessesLeft > 0) {
     alert('That\'s enough guessing. The correct answer is that I\'ve actually visited 9 stadiums outside of Seattle. I really need to travel more.')
   }
 }
+}
+q6();
 questionsAsked++;
 console.log('The user has answered ' + correctAnswers + ' out of ' + questionsAsked + ' questions correctly.');
 
 //Question #7
+
+function q7() {
 guessesLeft = 6;
 var guessNum = 0;
 
@@ -157,7 +185,11 @@ while (guessesLeft > 0) {
     alert('Never mind. In the immortal words of En Vogue, "you\'re never gonna get it." The teams whose stadiums I\'ve visited are the Athletics, Brewers, Cardinals, Giants, Indians, Padres, White Sox, Yankees, and most belovedly, the Cubs. Go Cubs, Go!')
   }
 }
+}
+q7();
+
 questionsAsked++;
 console.log('The user has answered ' + correctAnswers + ' out of ' + questionsAsked + ' questions correctly.');
 
 alert('You got ' + correctAnswers + ' answers correct out of ' + questionsAsked + ' questions I asked. But more importantly, you know a little more about me. Thanks for playing along!');
+>>>>>>> c5a5c769fe67f306dfec547bd0c751ac5e860762
